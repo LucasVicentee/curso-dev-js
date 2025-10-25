78e2f1be817704ad2f5eae30a4d0d032b91a8216
LucasVicentee <lh201612@gmail.com>
2025-10-25 08:18:54 -0300

    Adicionando os arquivos solicitados pelo professor em sala de aula

--- exercicios-individuais/JS/projetos/cadastro/cadastro.js
+++ exercicios-individuais/JS/projetos/cadastro/cadastro.js
@@ -1,14 +1,17 @@
-let usuarios = JSON.parse(localStorage.getItem("cadastro_usuarios")) || []; // Convertendo o armazenamento para JSON em um formato de Array e verificando se tem ou não um array
+let usuarios = JSON.parse(localStorage.getItem("cadastro_usuarios")) || [];
 
-// Telas
+//Telas
 const telaLista = document.querySelector("#tela-lista");
 const telaCadastro = document.querySelector("#tela-cadastro");
 
-// Botões
+//Botões
 const btnAdicionar = document.querySelector("#btn-adicionar");
 const btnVoltarLista = document.querySelector("#btn-voltar-lista");
+const btnDownload = document.querySelector("#btn-download");
+const btnUpload = document.querySelector("#btn-upload");
+const inputUpload = document.querySelector("#input-upload");
 
-// Inputs
+//Inputs
 const inputId = document.querySelector("#user-id");
 const inputNome = document.querySelector("#user-nome");
 const inputSobrenome = document.querySelector("#user-sobrenome");
@@ -23,27 +26,37 @@ const inputEstado = document.querySelector("#user-estado");
 const inputObs = document.querySelector("#user-obs");
 
 const form = document.querySelector("#user-form");
-
 const tabelaCorpo = document.querySelector("#user-table-body");
 
-// Funções das telas
-function mostrarTelaLista() {
-    telaLista.classList.remove("d-none"); // Removendo o d-none
-    telaCadastro.classList.add("d-none");
+let idEmEdicao = null;
 
+const btnCep = document.querySelector("#btn-buscar-cep");
+
+
+const inputBusca = document.querySelector("#user-busca");
+
+
+
+
+
+function mostrarTelaLista(){
+    telaLista.classList.remove("d-none");
+    telaCadastro.classList.add("d-none");
     renderizarTabela();
+    form.reset();
 }
 
-function mostrarTelaCadastro() {
-    telaCadastro.classList.remove("d-none"); // Adicionando o d-none
+function mostrarTelaCadastro(){
+    telaCadastro.classList.remove("d-none");
     telaLista.classList.add("d-none");
 }
 
-function salvarUsuario() {
+function salvarUsuario(){
     const id = Number(inputId.value);
     const nome = inputNome.value;
     const sobrenome = inputSobrenome.value;
     const email = inputEmail.value;
+    const cep = inputCep.value;
     const rua = inputRua.value;
     const numero = inputNumero.value;
     const complemento = inputComplemento.value;
@@ -53,20 +66,31 @@ function salvarUsuario() {
     const obs = inputObs.value;
 
     const usuario = {
-        id: id || Date.now(), nome, sobrenome, email, rua, numero, complemento, bairro, cidade, estado, obs
+        id: id || Date.now(), nome, sobrenome, email, cep, rua, numero, complemento, bairro, cidade, estado, obs
     }
 
-    usuarios.push(usuario);
+    if (idEmEdicao){
+        
+        const index = usuarios.findIndex(user => user.id === idEmEdicao); // index = 0; index = 2
+        if (index !== -1){
+            usuarios[index] = usuario;
+        }
+    }else{
+        usuarios.push(usuario);
+    }
+    
     salvarNoStorage();
+    form.reset();
+    mostrarTelaLista();
 }
 
-function salvarNoStorage() { // Função para salvar os dados do usuário no navegador
-    localStorage.setItem("cadastro_usuarios", JSON.stringify(usuarios));
+function salvarNoStorage(){
+    localStorage.setItem("cadastro_usuarios",JSON.stringify(usuarios));
 }
 
-function renderizarTabela() {
-    tabelaCorpo.innerHTML = ""; // Limpando a tabela toda
-    usuarios.forEach(user => {
+function renderizarTabela(usuariosFiltrados = usuarios){
+    tabelaCorpo.innerHTML = "";
+    usuariosFiltrados.forEach(user => {
         const tr = document.createElement("tr");
         tr.innerHTML = `
             <td>${user.nome}</td>
@@ -74,49 +98,142 @@ function renderizarTabela() {
             <td>${user.email}</td>
             <td>
                 <button type="button" class="btn btn-sm btn-warning" data-id="${user.id}">Editar</button>
+
                 <button type="button" class="btn btn-sm btn-danger" data-id="${user.id}">Excluir</button>
             </td>
         `;
 
         tabelaCorpo.appendChild(tr);
     });
-};
 
-function excluirUsuario(id) {
-    if (confirm("Você deseja realmente excluir esse usuário?")) {
+}
+
+function excluirUsuario(id){
+    if (confirm("Você deseja realmente excluir esse usuário?")){
         console.log(id);
-        const usuarios = usuarios.filter(user => user.id !== id);
-
+        usuarios = usuarios.filter(user => user.id !== id);
         salvarNoStorage();
         renderizarTabela();
     }
 }
 
-// Funcionalidade do botão com o click em qualquer uma das opções
-function inicializar() {
-    btnAdicionar.addEventListener("click", mostrarTelaCadastro);
-    btnVoltarLista.addEventListener("click", mostrarTelaLista);
+function editarUsuario(id){
+
+    const usuario = usuarios.find(user => user.id === id);
+
+    if (!usuario) return;
+
+    idEmEdicao = id;
+
+    inputId.value = usuario.id;
+    inputNome.value = usuario.nome;
+    inputSobrenome.value = usuario.sobrenome;
+    inputEmail.value = usuario.email;
+    inputCep.value = usuario.cep;
+    inputRua.value = usuario.rua;
+    inputNumero.value = usuario.numero;
+    inputComplemento.value = usuario.complemento;
+    inputBairro.value = usuario.bairro;
+    inputCidade.value = usuario.cidade;
+    inputEstado.value = usuario.estado;
+    inputObs.value = usuario.obs;
+
+    mostrarTelaCadastro()
+
+}
+
+async function buscarCEP(){
+
+    const cep = inputCep.value.replace(/\D/g,"");
+
+    if (cep.length === 8){
+
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+        const dados = await resposta.json();
+
+        // console.log(dados);
+
+        if (!dados.erro){
+            inputCep.value = dados.cep;
+            inputRua.value = dados.logradouro;
+            inputComplemento.value = dados.complemento;
+            inputBairro.value = dados.bairro;
+            inputCidade.value = dados.localidade;
+            inputEstado.value = dados.estado;
+        }else{
+            alert("CEP Inválido, tente novamente!");
+        }
+    }else{
+        alert("Verifique a quantidade de números digitados!");
+    }
+
+}
+
+function buscarUsuario(){
+    //toLowerCase => transforma tudo em minusculo
+    //trim => remove os espaços dos extremos.
+    const conteudo = inputBusca.value.toLowerCase().trim();
+
+    if (!conteudo){
+        renderizarTabela();
+        return;
+    }
+
+    const usuariosFiltrados = usuarios.filter(user => {
+        return user.nome.toLowerCase().trim().includes(conteudo) || user.sobrenome.toLowerCase().trim().includes(conteudo) || 
+        user.email.toLowerCase().trim().includes(conteudo);
+    });
+
+    console.log(usuariosFiltrados);
+
+    renderizarTabela(usuariosFiltrados);
+
+}
+
+function downloadArquivo(){
+    const dados = JSON.stringify(usuarios);
+    const arquivo = new Blob([dados], {type: "application/json"});
+    const url = URL.createObjectURL(arquivo);
+    const linkDownload = document.createElement("a");
+    linkDownload.href = url;
+    linkDownload.download = "usuarios.json";
+    linkDownload.click();
+    URL.revokeObjectURL(url);
+}
+
+function uploadArquivo(){
+
+}
+
+function inicializar(){
+    btnAdicionar.addEventListener("click",mostrarTelaCadastro);
+    btnVoltarLista.addEventListener("click",mostrarTelaLista);
+    btnCep.addEventListener("click",buscarCEP);
 
     form.addEventListener("submit", salvarUsuario);
+    mostrarTelaLista();
 
-    renderizarTabela();
+    inputBusca.addEventListener("input",buscarUsuario);
+
+    btnDownload.addEventListener("click", downloadArquivo);
+    btnUpload.addEventListener("click", () => inputUpload.click());
+    inputUpload.addEventListener("change", uploadArquivo);
 
     tabelaCorpo.addEventListener("click", (event) => {
         const target = event.target.closest("button");
-
         if(!target) return
-
+        
         const id = Number(target.dataset.id);
-
+        
         if (isNaN(id)) return
 
-        if (target.classList.contains("btn-warning")) {
+        if (target.classList.contains("btn-warning")){
             editarUsuario(id);
-        }
-        else if (target.classList.contains("btn-danger")) {
+        } else if (target.classList.contains("btn-danger")){
             excluirUsuario(id);
-        }
-    });
+        }        
+    })
 }
 
-inicializar(); // Chamada da função
+inicializar();
